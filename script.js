5297d88ebdb619da7c7f4512d89c0a4abd77f2d9
shwetakul2005 <155085867+shwetakul2005@users.noreply.github.com>
2025-07-28 22:51:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const input_box = document.getElementById("text_box")
+const list_container = document.getElementById("task_items")
+
+function add_task(){
+    if(input_box.value === ""){
+        alert("Add a task!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input_box.value;
+        list_container.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "🗑️";
+        li.appendChild(span);
+    }
+    input_box.value="";
+    saveData();
+}
+
+list_container.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+
+function saveData(){
+    localStorage.setItem("data", list_container.innerHTML);
+}
+function showTask(){
+    list_container.innerHTML = localStorage.getItem("data");
+}
+showTask();
