c9d3fc170a589232d304a3de9cad81f8a18731f1
shwetakul2005 <155085867+shwetakul2005@users.noreply.github.com>
2025-08-01 01:21:42 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const input_box = document.getElementById("text_box")
 const list_container = document.getElementById("task_items")
 
@@ -35,42 +34,4 @@ function saveData(){
 function showTask(){
     list_container.innerHTML = localStorage.getItem("data");
 }
-=======
-const input_box = document.getElementById("text_box")
-const list_container = document.getElementById("task_items")
-
-function add_task(){
-    if(input_box.value === ""){
-        alert("Add a task!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = input_box.value;
-        list_container.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = "🗑️";
-        li.appendChild(span);
-    }
-    input_box.value="";
-    saveData();
-}
-
-list_container.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked")
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false)
-
-function saveData(){
-    localStorage.setItem("data", list_container.innerHTML);
-}
-function showTask(){
-    list_container.innerHTML = localStorage.getItem("data");
-}
->>>>>>> 73bb4ae ( CSS changes)
 showTask();
